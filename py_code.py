89ad43b25a8032da1ead27db016fc8eea202743e
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2021-08-02 09:16:08 +0200

    fix clipboard
    clipster produced segfaults

--- py_code.py
+++ py_code.py
@@ -174,15 +174,16 @@ data=nvim.eval('@"').encode(encoding="utf8")
 displays=pylib.list_utils.uniq([i['x_server'] for i in pylib.screen_utils.parse_screen_layout_env_var_v3()])
 for display in displays:
     os.environ['DISPLAY']=display
-    p=subprocess.Popen(["clipster","-c"],stdin=subprocess.PIPE,env=os.environ)
+    cmd=["xclip", "-selection", "clipboard"] # 'primary' is the middle mouse one 'clipboard' the ctrl-v one
+    p=subprocess.Popen(cmd,stdin=subprocess.PIPE,env=os.environ)
     p.communicate(input=data)
     if p.returncode != 0 and re.match("\S+:[0-9.]+",display):
         display = ":"+ display.split(":")[1]
         os.environ['DISPLAY']=display
-        p=subprocess.Popen(["clipster","-c"],stdin=subprocess.PIPE,env=os.environ)
+        p=subprocess.Popen(cmd,stdin=subprocess.PIPE,env=os.environ)
         p.communicate(input=data)
         if not p.returncode == 0:
-            raise subprocess.CalledProcessError() # TODO: add args
+            raise subprocess.CalledProcessError(p.returncode,cmd) # TODO: add args
 """
 pycode.add_pycode( name , code ,uses_vim_vars=[],doc=doc)
 
