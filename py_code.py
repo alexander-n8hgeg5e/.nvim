3771cfb44edc3ef957da3266a9fa362a1d9cd216
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2023-09-30 01:35:01 +0200

    fold it

--- py_code.py
+++ py_code.py
@@ -260,4 +260,4 @@ for tmux_session in tmux_sessions:
 """
 pycode.add_pycode( name , code ,uses_vim_vars=['tmux_socket'],doc=doc )
 
-# vim: set syntax=py_worker_code     foldmethod=syntax :
+# vim: set syntax=py_worker_code     foldmethod=syntax foldlevel=0:
