f57fa83f26d55c4079a629d18c6cafa331ab1bbc
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2023-11-26 19:37:31 +0100

    correct encoding name

--- py_code.py
+++ py_code.py
@@ -199,7 +199,7 @@ name='clipboard_update_xorg_s'
 doc="""only python >= 3.6"""
 code="""
 clipboard_file_path=os.environ['CLIPBOARD_FILE']
-data=nvim.eval('@"').encode(encoding="utf8")
+data=nvim.eval('@"').encode(encoding="utf-8")
 displays=pylib.list_utils.uniq([i['x_server'] for i in pylib.screen_utils.parse_screen_layout_env_var_v3()])
 for display in displays:
     os.environ['DISPLAY']=display
@@ -220,7 +220,7 @@ name='clipboard_update_xorg_p'
 doc="""only python >= 3.6"""
 code="""
 clipboard_file_path=os.environ['CLIPBOARD_FILE']
-data=nvim.eval('@"').encode(encoding="utf8")
+data=nvim.eval('@"').encode(encoding="utf-8")
 displays=pylib.list_utils.uniq([i['x_server'] for i in pylib.screen_utils.parse_screen_layout_env_var_v3()])
 for display in displays:
     os.environ['DISPLAY']=display
